c7b83e37b9256253acbf98b5753e1ace4b569ec4
daves <davespink290@gmail.com>
2024-09-23 14:18:21 -0600

    think i solved the rotation issue

--- uploadincitio.php
+++ uploadincitio.php
@@ -1,63 +1,87 @@
 <?php
- 
-if(isset($_GET['dir'])) {
+
+if (isset($_GET['dir'])) {
     $dir = $_GET['dir'];
 }
 
-if(!is_dir($dir)){
-  echo 'error no directory ';
+if (!is_dir($dir)) {
+    echo 'error no directory ';
     die($dir);
 }
 
 global $uploaded;
 
-function cropAndRename($fileName,$file_extension)
+function cropAndRename($fileName, $file_extension)
 {
-  //  echo "filename - " . $fileName;
+    //  echo "filename - " . $fileName;
+
+    $exif_info =  exif_read_data($fileName);
+
+
+    // read orientation, my camera returns 6.
+
     global $dir;
     global $uploaded;
-    $stamp = time(); 
- 
+    $stamp = time();
+
     $uploaded = $dir . '/img_' . $stamp  . '.' . $file_extension;
-  //  $thumb = $dir . '/tn_img_' . $stamp  . '.jpg';
-    rename($fileName,$uploaded);
+    //  $thumb = $dir . '/tn_img_' . $stamp  . '.jpg';
+    rename($fileName, $uploaded);
 
     // is this ok
-    if($file_extension=='jpg')
-         $img = imagecreatefromjpeg($uploaded);
-        else
+    if ($file_extension == 'jpg')
+        $img = imagecreatefromjpeg($uploaded);
+    else
         $img = imagecreatefrompng($uploaded);
-    //list($width, $height) = getimagesize($uploaded);
+
+
+
+
+    // $exif_info = var_dump(exif_read_data($filename));
+
+
+    // read orientation, my camera returns 6.
+
+    /* 
+
+
+1 = 0 degrees: the correct orientation, no adjustment is required.
+2 = 0 degrees, mirrored: image has been flipped back-to-front.
+3 = 180 degrees: image is upside down.
+4 = 180 degrees, mirrored: image has been flipped back-to-front and is upside down.
+5 = 90 degrees: image has been flipped back-to-front and is on its side.
+6 = 90 degrees, mirrored: image is on its side.
+7 270 degrees: image has been flipped back-to-front and is on its far side.
+8 270 degrees, mirrored: image is on its far side.
+
+
+
+
+*/
+
+
 
     $width = imagesx($img);
     $height = imagesy($img);
 
-//    $new_width =   80; //$width * 0.5;
- //   $new_height = $height * $new_width / $width;
- //   $newImg = imagecreatetruecolor($new_width, $new_height);
-
-  //  imagecopyresampled($newImg, $img, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
-  //  imagejpeg($newImg, $thumb);
-
 
     // now resize the image
-    if($width>500){
+    if ($width > 500) {
         $new_width =   500; //$width * 0.5;
         $new_height = $height * $new_width / $width;
         $newImg = imagecreatetruecolor($new_width, $new_height);
 
         imagecopyresampled($newImg, $img, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
 
-        if($file_extension=='jpg')
-            imagejpeg($newImg, $uploaded);
-        elseif($file_extension=='png')
-            imagepng($newImg, $uploaded);
-  
+        $rot=imagerotate($newImg,270,0);
+
+        if ($file_extension == 'jpg')
+            imagejpeg($rot, $uploaded);
+        elseif ($file_extension == 'png')
+            imagepng($rot, $uploaded);
     }
 
     imagedestroy($img);
-     
- 
 }
 
 
@@ -79,12 +103,11 @@ if (isset($_FILES['file']['name'])) {
     if (in_array($file_extension, $valid_ext)) {
         // Upload file
         if (move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
-            cropAndRename($location,$file_extension);
-             
+            cropAndRename($location, $file_extension);
         }
     }
- 
-   echo $uploaded;
- 
+
+    echo $uploaded;
+
     exit;
 }
