7b0a7a54529af870bc104dcc947cbe3f1955565f
daves <davespink290@gmail.com>
2024-10-22 17:06:30 -0600

    Before general clean up

---
+++ dirfunc.php
@@ -0,0 +1,23 @@
+<?php
+$user = $_GET['user'];
+echo $user;
+ 
+$file = "0.jpeg";
+
+@mkdir("users");
+@mkdir("users/" . $user);
+@mkdir("users/" . $user . "/photos");
+ 
+copy("photos/" . $file , "users/" . $user . "/photos/" . $file);
+
+// iterate directory
+$dir = new DirectoryIterator("photos");
+foreach ($dir as $fileinfo) {
+    if (!$fileinfo->isDot()) {
+        $file = ($fileinfo->getFilename());
+        var_dump($file);
+        copy("photos/" . $file , "users/" . $user . "/photos/" . $file);
+    }
+}
+   
+?>
